84db0f4c617585dcbfb61523915026f22af20ea2
s.lim <sam@typhonic.io>
2023-07-27 18:33:19 -0700

    Add docstring to app fixture, add fixture for app.test_client()

--- app/api/conftest.py
+++ app/api/conftest.py
@@ -6,5 +6,12 @@ from api import create_app
 @pytest.fixture
 # TODO: Add typing hint (flask.Flask ?)
 def app():
+    """Fixture for creating Flask app"""
     app = create_app(test_config="api.config.TestingConfig")
     yield app
+
+
+@pytest.fixture
+def client(app):
+    """Fixture for accessing client instance in tests"""
+    return app.test_client()
--- app/api/tests/test_fixtures.py
+++ app/api/tests/test_fixtures.py
@@ -2,4 +2,6 @@ import pytest
 
 
 def test_create_app_fixture(app):
-    assert app
+    breakpoint()
+    assert app is not None
+    assert app.name == "api"
