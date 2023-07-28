3eb81e7a6c15520a68891e5c73ef18a9e79f16eb
s.lim <sam@typhonic.io>
2023-07-27 18:34:42 -0700

    Add test for app client fixture, passes

--- app/api/tests/test_fixtures.py
+++ app/api/tests/test_fixtures.py
@@ -2,6 +2,10 @@ import pytest
 
 
 def test_create_app_fixture(app):
-    breakpoint()
     assert app is not None
     assert app.name == "api"
+
+
+def test_app_client_fixture(client):
+    assert client is not None
+    assert client.application.name == "api"
